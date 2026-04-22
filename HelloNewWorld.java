9f438547adb8d0719dc1286ac94b44c378c01326
Abhishek612 <patel.abhishek15@gmail.com>
2026-04-22 19:16:03 +0530

    Update HelloNewWorld.java

--- HelloNewWorld.java
+++ HelloNewWorld.java
@@ -1,5 +1,5 @@
 public class HelloNewWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello World from Abhishek. I am learn about Jenkins !");
     }
 }
